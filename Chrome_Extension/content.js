6153c928f1d5816569a05695325800f169a576e6
Paul Lam <paullam328@gmail.com>
2019-09-14 13:04:37 -0700

    chrome keypress detection added

--- Chrome_Extension/content.js
+++ Chrome_Extension/content.js
@@ -1,3 +1,4 @@
+
 // content.js
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -9,5 +10,24 @@ chrome.runtime.onMessage.addListener(
         // This line is new!
         chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
       }
+
+      alert("Hiii");
     }
   );
+
+  chrome.commands.onCommand.addListener(function(command) {
+    if (command === "check-codesmell") {
+      alert("check-codesmell!");
+  } 
+});
+
+//Manifest.json:
+    /*
+    "content_scripts": [
+      {
+        "matches": [
+          "<all_urls>"
+        ],
+        "js": ["jquery-3.4.1.min.js", "content.js"]
+      }
+    ],*/
