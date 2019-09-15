ec10cbfa2bb7e66c8049212242a4397548edc0e8
Vivian Shao <xigu95@gmail.com>
2019-09-14 21:33:42 -0700

    refactor

--- Chrome_Extension/background.js
+++ Chrome_Extension/background.js
@@ -32,19 +32,20 @@ var message = {result: []}
 function doParse(aCode){
     var aObj = [];
     var iObj = 0;
-    for(var i = 0; i<aCode.length; i++){
-        var sCode = aCode[i];
+    for(var iLineNumber = 0; iLineNumber<aCode.length; iLineNumber++){
+        var sCode = aCode[iLineNumber];
         // its a comment
         if(sCode.includes("//")) continue;
         if(sCode.includes("console")) continue;
         if(sCode.includes("var")){
-            createVar(sCode, aObj, i, iObj++);
+            createObj(sCode, aObj, "var", iLineNumber, iObj++);
         }else if(sCode.includes("function")){
-            createFunction(sCode, aObj, i, iObj++);
+            createObj(sCode, aObj, "function", iLineNumber, iObj++);
+            findFunctionScope();
         }else if(sCode.includes("const")){
-            createConst(sCode, aObj, i, iObj++);
+            createObj(sCode, aObj, "const", iLineNumber, iObj++);
         }else{
-            updateUsage(sCode, aObj, i, iObj);
+            updateUsage(sCode, aObj, iLineNumber, iObj);
         }
     }
     //alert(aCode);
@@ -53,7 +54,9 @@ function doParse(aCode){
     console.log(smell_detector(JSON.stringify(json)));
     alert("HI");
 }
-function updateUsage(sCode, aObj, i, iObj){
+function findFunctionScope(){
+}
+function updateUsage(sCode, aObj, iLineNumber, iObj){
     var aCode = sCode.split(/\s+/);
     for(var iCode=0; iCode<aCode.length; iCode++){
         for(var iObj = 0; iObj<aObj.length; iObj++){
@@ -61,7 +64,7 @@ function updateUsage(sCode, aObj, i, iObj){
             var oObj = aObj[iObj];
             if(oObj["name"] == oCode){
                 var size = oObj.usages.length;
-                oObj.usages[size] = i;
+                oObj.usages[size] = iLineNumber+1;
             }
         }
     }
@@ -70,7 +73,25 @@ function createObj(sCode, aObj, sType, iLineNumber, iObjCount){
     var sName = getName(sCode, sType);
     var aUsage = [];
     aUsage[0] = iLineNumber;
-    aObj[iObjCount] = new Obj(sName, sType, i, aUsage, 1);
+    sType = (sType == "var")? "variable":sType;
+    sType = (sType == "const")? "constant":sType;
+
+    aObj[iObjCount] = (sType != "function")? new Obj(sName, sType, iLineNumber+1, aUsage, 1): createFunc(sName, sType, iLineNumber+1, aUsage, 1, sCode);
+}
+function createFunc(sName, sType, iLineNumber, aUsage, iLineCount, sCode){
+    var sParam = getParam(sCode, sType);
+    return new Func(sName, sType, iLineNumber+1, aUsage, 1, iLineNumber+1, iLineNumber+1, -1, false, sParam);
+}
+
+function getParam(sCode, sType){
+    if(sType == "function"){
+        var searchTerm = "(";
+        var iOpen = sCode.indexOf(searchTerm);
+        var iClose = sCode.indexOf(")");
+
+        return sCode.substring(iOpen+1, iClose);
+    }
+    return "";
 }
 
 function getName(sCode, sType){
@@ -82,67 +103,27 @@ function getName(sCode, sType){
             break;
         }
     }
+    sName = (sType == "function")? sType.split("(")[0]:sName;
     return sName;
-}
-function createVar(sCode, aObj, i, iObj){
-    // standard convention for declaring a variable is
-    // var x;
-    var aCode = sCode.split(/\s+/);
-    var sName = "";
-    for(var i=0; i<aCode.length; i++){
-        if(i>0 && aCode[i-1] == "var"){
-            sName = aCode[i];
-            break;
-        }
-    }
-    var aUsage = [];
-    aUsage[0] = i;
-    aObj[iObj] = new Obj(sName,"variable", i, aUsage, 1);
-}
-function createConst(sCode, aObj, i, iObj){
-    // standard convention for declaring a variable is
-    // var x;
-    var aCode = sCode.split(/\s+/);
-    var sName = "";
-    for(var i=0; i<aCode.length; i++){
-        if(i>0 && aCode[i-1] == "const"){
-            sName = aCode[i];
-            break;
-        }
-    }
-    var aUsage = [];
-    aUsage[0] = i;
-    aObj[iObj] = new Obj(sName,"constant", i, aUsage, 1);
-}
-function createFunction(sCode, aObj, i, iObj){
-    var aCode = sCode.split(/\s+/);
-    var sName = "";
-    for(var i=0; i<aCode.length; i++){
-        if(i>0 && aCode[i-1] == "function"){
-            sName = aCode[i];
-            break;
-        }
-    }
-    var aUsage = [];
-    aUsage[0] = i;
-    aObj[iObj] = new Obj(sName,"function", i, aUsage, 1);
 }
 function Obj(name, type, declaration, usage, lineNum) {
   this.name = name;
   this.type = type;
   this.declaration = declaration;
   this.usages = usage;
-  this.lineNum = lineNum;
+  this.lineCount = lineNum;
 }
 
-function Func(name, type, declaration, usage, start,end,returnLine,hasCatch,param) {
-  Obj.call(name, type, declaration, usage);
+function Func(name, type, declaration, usage, lineNum, start, end, returnLine, hasCatch, param) {
+  Obj.call(this, name, type, declaration, usage, lineNum);
+
   this.start = start;
   this.end = end;
   this.returnLine = returnLine;
   this.hasCatch = hasCatch;
-  this.param = param;
+  this.params = param;
 }
+
 Func.prototype = Object.create(Obj.prototype);
 Func.prototype.constructor = Func;
 
