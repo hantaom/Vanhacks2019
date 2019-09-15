dcb3acd8c13c51af7f5681ff4b06316b4f1b7bfd
Vivian Shao <xigu95@gmail.com>
2019-09-14 19:22:04 -0700

    parser with simple variables and function

--- Chrome_Extension/background.js
+++ Chrome_Extension/background.js
@@ -19,18 +19,78 @@
         console.log("FirstDebug: " + linesOfCode[i].outerText);
       }
 
-      //UNCOMMENT:
-      //doParse(strsOfCode);
-    
-    });
-  
+//      alert(strsOfCode);
+      doParse(strsOfCode);
+});
+function doParse(aCode){
+    var aObj = [];
+    var iObj = 0;
+    for(var i = 0; i<aCode.length; i++){
+        var sCode = aCode[i];
+        // its a comment
+        if(sCode.includes("//")) continue;
+        if(sCode.includes("var")){
+            createVar(sCode, aObj, i, iObj++);
+        }else if(sCode.includes("function")){
+            createFunction(sCode, aObj, i, iObj++);
+        }
 
-    
+    }
+    alert(aCode);
+}
+function createVar(sCode, aObj, i, iObj){
+    // standard convention for declaring a variable is
+    // var x;
+    var aCode = sCode.split(/\s+/);
+    var sName = "";
+    for(var i=0; i<aCode.length; i++){
+        if(i>0 && aCode[i-1] == "var"){
+            sName = aCode[i];
+            break;
+        }
+    }
+    var aUsage = [];
+    aUsage[0] = i;
+    aObj[iObj] = new Obj(sName,"variable", i, aUsage, 1);
+}
+function createFunction(sCode, aObj, i, iObj){
+    console.log(iObj);
+    var aCode = sCode.split(/\s+/);
+    var sName = "";
+    for(var i=0; i<aCode.length; i++){
+        if(i>0 && aCode[i-1] == "function"){
+            sName = aCode[i];
+            break;
+        }
+    }
+    var aUsage = [];
+    aUsage[0] = i;
+    aObj[iObj] = new Obj(sName,"function", i, aUsage, 1);
+}
+function Obj(name, type, declaration, usage, lineNum) {
+  this.name = name;
+  this.type = type;
+  this.declaration = declaration;
+  this.usage = usage;
+  this.lineNum = lineNum;
+}
+
+function Func(name, type, declaration, usage, start,end,returnLine,hasCatch,param) {
+  Obj.call(name, type, declaration, usage);
+  this.start = start;
+  this.end = end;
+  this.returnLine = returnLine;
+  this.hasCatch = hasCatch;
+  this.param = param;
+}
+Func.prototype = Object.create(Obj.prototype);
+Func.prototype.constructor = Func;
+
+
 chrome.commands.onCommand.addListener(function(command) {
   if (command === "check-codesmell") {
 
   chrome.tabs.getSelected(null, function(tab) {
-  
     // Now inject a script onto the page
     chrome.tabs.executeScript(tab.id, {
          code: "chrome.extension.sendRequest({content: document.body.innerHTML}, function(response) { console.log('success'); });"
